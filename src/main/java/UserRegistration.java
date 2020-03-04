99604531a7070362ebb6ca5825a477c0e0853e51
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-04 16:57:18 +0530

    ADDED : Test method to validate first name

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,5 +1,13 @@
+import java.util.regex.Pattern;
+
 public class UserRegistration
 {
+    public String firstNamePattern="^[A-Z][a-zA-Z]{2,}$";
+    public boolean validateFirstName(String firstName)
+    {
+        return (Pattern.matches(firstNamePattern,firstName));
+    }
+
     public static void main(String[] args)
     {
         System.out.println("********** WELCOME TO USER REGISTRATION SYSTEM **********");
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,6 +1,45 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest
 {
+    //TEST CASE FOR VALID FIRST NAME WITH FIRST LETTER CAPITAL
+    @Test
+    public void givenFirstName_whenValidUpperCase_thenReturn()
+    {
+        String fname="Vaishnavi";
+        UserRegistration validate=new UserRegistration();
+        boolean result=validate.validateFirstName(fname);
+        Assert.assertTrue(result);
+    }
 
+    //TEST CASE FOR INVALID FIRST NAME WITH LOWER CASE
+    @Test
+    public void givenFirstName_whenInvalidUpperCase_thenReturn()
+    {
+        String fname="vaishnavi";
+        UserRegistration validate = new UserRegistration();
+        boolean result = validate.validateFirstName(fname);
+        Assert.assertFalse(result);
+    }
+
+    //TEST CASE FOR VALID FIRST NAME WITH MINIMUM THREE LETTERS
+    @Test
+    public void givenFirstName_whenValidGreaterThanThree_thenReturn()
+    {
+        String fname="Vaish";
+        UserRegistration validate = new UserRegistration();
+        boolean result = validate.validateFirstName(fname);
+        Assert.assertTrue(result);
+    }
+
+    //TEST CASE FOR INVALID FIRST NAME WITH LESS THAN THREE CHARACTERS
+    @Test
+    public void givenFirstName_whenInvalidGreaterThanThree_thenReturn()
+    {
+        String fname="Va";
+        UserRegistration validate = new UserRegistration();
+        boolean result = validate.validateFirstName(fname);
+        Assert.assertFalse(result);
+    }
 }
